e6aee0724601f344d23cbfe40516b0cb91149460
wingrid330 <wingrid@seas.upenn.edu>
2025-06-27 19:58:12 -0400

    Update connect_to_eth.py

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -31,9 +31,8 @@ def connect_with_middleware(contract_json):
 	# The second section requires you to inject middleware into your w3 object and
 	# create a contract object. Read more on the docs pages at https://web3py.readthedocs.io/en/stable/middleware.html
 	# and https://web3py.readthedocs.io/en/stable/web3.contract.html
-  # Inject middleware (required for BNB testnet's consensus mechanism)
-  w3.middleware_onion.inject(geth_poa_middleware, layer=0)
-
+  	# Inject middleware (required for BNB testnet's consensus mechanism)
+  	w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 	contract = w3.eth.contract(address=address, abi=abi)
 
 	return w3, contract
